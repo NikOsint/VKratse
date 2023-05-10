5fa7e9c614d8a833728a1f6d23c9e0a52f0cb381
Nikolay Osintsev <niosintsev@miem.hse.ru>
2023-05-10 04:18:50 +0300

    implement user class

--- VKratseClient/include/user.h
+++ VKratseClient/include/user.h
@@ -2,7 +2,23 @@
 // Created by nikosint on 10.05.23.
 //
 
-#ifndef VKRATSECLIENT_VKRATSEUSER_H
-#define VKRATSECLIENT_VKRATSEUSER_H
+#ifndef VKRATSECLIENT_USER_H
+#define VKRATSECLIENT_USER_H
 
-#endif //VKRATSECLIENT_VKRATSEUSER_H
+#include <QString>
+
+class VKratseUser {
+public:
+  VKratseUser(QString login, QString name, QString surname);
+  ~VKratseUser();
+
+  QString getLogin() const;
+  QString getName() const;
+  QString getSurname() const;
+  QString getNameSurname() const;
+
+private:
+  QString login, name, surname;
+};
+
+#endif //VKRATSECLIENT_USER_H
--- VKratseClient/src/user.cpp
+++ VKratseClient/src/user.cpp
@@ -1,3 +1,30 @@
 //
 // Created by nikosint on 10.05.23.
 //
+
+#include "user.h"
+
+#include <utility>
+
+VKratseUser::VKratseUser(QString login, QString name, QString surname)
+  : login(std::move(login)),
+    name(std::move(name)),
+    surname(std::move(surname)) {}
+
+VKratseUser::~VKratseUser() = default;
+
+QString VKratseUser::getLogin() const {
+  return login;
+}
+
+QString VKratseUser::getName() const {
+  return name;
+}
+
+QString VKratseUser::getSurname() const {
+  return surname;
+}
+
+QString VKratseUser::getNameSurname() const {
+  return name + " " + surname;
+}
